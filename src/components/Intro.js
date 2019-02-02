0f8b43919187ff97fbcbdaccd1888d2ab7adce92
Kin Chan <xkinchanx@gmail.com>
2019-02-02 10:30:24 -0800

    [prog] intro page with message display none upon browser width < 700px

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,10 +1,30 @@
 import React from 'react';
 import styled from 'styled-components';
 
+import { StandardComponentStyles } from './CoreStyles';
+
 const Intro = (props) =>
-  <div>
-    <h2>Kin Chan</h2>
-    <p>The software engineer you've been searching for</p>
-  </div>
+  <IntroContainer id="top">
+    <h2 className="name">Kin Chan</h2>
+    <p className="statement"><em>The software engineer you've been searching for</em></p>
+  </IntroContainer>
+
+const IntroContainer = styled.div`
+  ${StandardComponentStyles}
+  background-image: url("https://wallpapers.wallhaven.cc/wallpapers/full/wallhaven-138815.jpg");
+  background-size: cover;
+  color: white;
+  font-size: 3em;
+  flex-direction: column;
+  text-shadow: 5px 5px 10px black;
+  > h2 {
+    margin: 0px;
+  }
+  > p {
+    @media (max-width: 700px) {
+      display: none;
+    }
+  }
+`;
 
 export default Intro;
