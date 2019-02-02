973e4a974aebbf526427b9028b2fac015173d004
Kin Chan <xkinchanx@gmail.com>
2019-02-01 21:54:27 -0800

    [feat] introduction component

---
+++ src/components/Intro.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Intro = (props) =>
+  <div>
+    <h2>Kin Chan</h2>
+    <p>The software engineer you've been searching for</p>
+  </div>
+
+export default Intro;
