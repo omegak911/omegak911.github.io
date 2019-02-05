52ca539aef74bbdecbd94c6a973e73bdc33f36ee
Kin Chan <xkinchanx@gmail.com>
2019-02-04 22:48:08 -0800

    [style] a tag and nac bar spacing

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,22 +1,6 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
 
-// const Nav = (props) =>
-//   <NavContainer>
-//     <DrawerTab>
-//       <hr/>
-//       <hr/>
-//       <hr/>
-//     </DrawerTab>
-//     <NavDrawer>
-//       <a href="#top">Top</a>
-//       <a href="#applications">Applications</a>
-//       <a href="#tech">TechStack</a>
-//       <a href="#blog">Blog</a>
-//       <a href="#contact">Contact</a>
-//     </NavDrawer>
-//   </NavContainer>
-
 class Nav extends Component {
   constructor(props) {
     super(props);
@@ -32,14 +16,12 @@ class Nav extends Component {
     return (
       <NavContainer>
         <DrawerTab onMouseEnter={this.toggleDrawer} style={{ display: showDrawer ? 'none' : 'flex'}}>
-          <hr/>
-          <hr/>
-          <hr/>
+          Nav
         </DrawerTab>
         <NavDrawer onMouseLeave={this.toggleDrawer} style={{ display: showDrawer ? 'flex' : 'none'}}>
           <a href="#top">Top</a>
-          <a href="#applications">Applications</a>
           <a href="#tech">TechStack</a>
+          <a href="#applications">Applications</a>
           <a href="#blog">Blog</a>
           <a href="#contact">Contact</a>
         </NavDrawer>
@@ -55,17 +37,23 @@ const DrawerTab = styled.div`
   height: 20px;
   width: 50px;
   flex-direction: column;
-  & hr {
-    margin: 2px 0px;
-  }
+  color: white;
+  text-align: center;
 `;
 
 const NavDrawer = styled.div`
-  display: none;
+  justify-content: space-evenly;
+  align-items: center;
   width: 100%;
   background: linear-gradient(to bottom, rgba(19,19,19,1) 0%,rgba(17,17,17,1) 14%,rgba(17,17,17,1) 14%,rgba(0,0,0,1) 75%,rgba(89,89,89,1) 98%,rgba(76,76,76,1) 100%);
   height: 100%;
   z-index: 99999;
+  & a {
+    text-decoration: none;
+    font-size: 1.5em;
+    color: white;
+    font-weight: 500;
+  }
 `;
 
 const NavContainer = styled.div`
