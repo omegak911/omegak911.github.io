e88f7352a4449afe461f9b9a811ed1d0e7c7a1be
Kin Chan <xkinchanx@gmail.com>
2019-02-18 17:45:28 -0800

    [feat] tech component

---
+++ src/components/TechStack.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+
+class TechStack extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    
+    }
+  }
+
+  render(){
+    return (
+      <div></div>
+    )
+  }
+}
+
+export default TechStack;
