3ee8ede90fcc5e0371da0241677f2e481983fb6e
Kin Chan <xkinchanx@gmail.com>
2019-04-04 21:50:52 -0700

    [refactor] deployed url down for group project

--- src/components/Applications.js
+++ src/components/Applications.js
@@ -13,7 +13,6 @@ class Applications extends Component {
           description: 'Rhyme visualization tool',
           image: 'https://media.licdn.com/media-proxy/ext?w=800&h=800&f=n&hash=IPlKHIIm9lFrnbt8npUC243t14s%3D&ora=1%2CaFBCTXdkRmpGL2lvQUFBPQ%2CxAVta5g-0R6jnhodx1Ey9KGTqAGj6E5DQJHUA3L0CHH05IbfPWjhfsCLerbzrUBHcS8HjQAxKum1SGG3E464KojnKN0j2Ze3JMT5agYUbhl4lWdI',
           title: 'Rhyme Doctor',
-          url: 'https://rhymedoctor.fun/',
           special: [
             ['Android Link', 'https://play.google.com/store/apps/details?id=com.notoriousnpm.rhymedoctor'],
             ['Android Codebase', 'https://github.com/Notorious-NPM/Rhyme-Doctor-Mobile']
