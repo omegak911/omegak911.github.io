555952d43f606629028a062e4716c9f599ab0bd8
Kin Chan <xkinchanx@gmail.com>
2019-06-24 16:33:03 -0700

    [feat] email form submission via 3rd party

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,7 +5,6 @@ import { StandardComponentStyles } from './CoreStyles';
 
 import GithubIcon from '../assets/GitHub.png';
 import LinkedInIcon from '../assets/LinkedIn.png';
-import GmailIcon from '../assets/Gmail.png';
 
 const Contact = (props) =>
   <ContactContainer id="contact">
@@ -14,13 +13,16 @@ const Contact = (props) =>
       <a href="https://github.com/omegak911" target="_blank" rel="noopener noreferrer">
         <img src={GithubIcon} alt="github"/>
       </a>
-      <a href="mailto:xkinchanx@gmail.com">
-        <img src={GmailIcon} alt="gmail"/>
-      </a>
       <a href="https://www.linkedin.com/in/thekinchan/">
         <img src={LinkedInIcon} alt="linkedIn"/>
       </a>
     </div>
+    <StyledForm action="https://formspree.io/kinchan1287@gmail.com" method="POST">
+      <input type="text" name="email" placeholder="your email" />
+      <input type="text" name="subject" placeholder="subject" />
+      <textarea type="text" name="message" placeholder="message" />
+      <button type="submit">send</button>
+    </StyledForm>
   </ContactContainer>
 
 const ContactContainer = styled.div`
@@ -35,5 +37,22 @@ const ContactContainer = styled.div`
       width: auto;
     } 
   }
-`
+`;
+
+const StyledForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  width: 300px;
+  & input {
+    font-size: 1.1em;
+    padding: 3px;
+    text-align: center;
+  }
+  & textarea {
+    font-size: 1.1em;
+    min-height: 200px;
+    text-align: center;
+  }
+`;
+
 export default Contact;
