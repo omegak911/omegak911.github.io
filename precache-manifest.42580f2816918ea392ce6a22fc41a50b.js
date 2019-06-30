b9484833d1876081e675893dc07439073182484d
Kin Chan <xkinchanx@gmail.com>
2019-06-30 00:05:40 -0700

    Updates

---
+++ precache-manifest.42580f2816918ea392ce6a22fc41a50b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f943ebdb874d9f73e4da958b27ebc484",
+    "url": "/static/media/photo.f943ebdb.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0547a2f59c8db083bab1",
+    "url": "/static/js/main.0547a2f5.chunk.js"
+  },
+  {
+    "revision": "032edc4b07b6728035e4",
+    "url": "/static/js/1.032edc4b.chunk.js"
+  },
+  {
+    "revision": "f540593143f5525b1de8fe5ac5e56883",
+    "url": "/index.html"
+  }
+];
